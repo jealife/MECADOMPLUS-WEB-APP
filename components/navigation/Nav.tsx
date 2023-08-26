3e4d521bf6d322ed8dcdd4c4fa0b23ee72796cc6
JEaLiFe jeaguy <jeanguylanememiaghe@gmail.com>
2023-08-26 21:58:57 +0000

    modified:   components/navigation/Nav.tsx

--- components/navigation/Nav.tsx
+++ components/navigation/Nav.tsx
@@ -12,34 +12,44 @@ const navLinks = [
     { name: 'Cards', href: '/cards' },
     { name: 'Admin', href: '/admin' },
 ]
+interface NavProps {
+    // définissez les propriétés de votre composant ici
+}
 
-export default function Nav() {
-    const state = { clicked: false };
-    const menu = () => {
-        this.setState({ clicked: !this.state.clicked })
+export default class Nav extends React.Component<NavProps> {
+    constructor(props: NavProps) {
+        super(props);
+        this.state = { clicked: false };
+        this.menu = this.menu.bind(this);
     }
-    return (
-        <nav className="flex items-center justify-center p-4 gap-2">
-            <Link href="/" className="logo">
-            <Image src={logo} 
-                    alt="Logo Mecadom" />
-            </Link>
-
-
-            <div className="menu-button_menu" onClick={() => menu()}>
-                {/* <IonIcon name="menu-outline"></IonIcon> */}
-            </div>
-
-            <div className="menu">
-                <div className="links-menu flex gap-4">
-                    <Navigation navLinks={navLinks} />
+    menu() { 
+        this.setState({ clicked: !this.state.clicked }); 
+    }
+    render(): React.ReactNode {
+        
+        return (
+            <nav className="flex items-center justify-center p-4 gap-2">
+                <Link href="/" className="logo">
+                <Image src={logo} 
+                        alt="Logo Mecadom" />
+                </Link>
+    
+    
+                <div className="menu-button_menu" onClick={()=>menu()}>
+                    {/* <IonIcon name="menu-outline"></IonIcon> */}
                 </div>
-
-                <div className="nav-btn">
-                    <Link href="./pages/register/sgnin.html" className="login">Se connecter</Link>
-                    <Link href="./pages/profile/profile.html" className="register hidden">Profile</Link>
+    
+                <div className="menu">
+                    <div className="links-menu flex gap-4">
+                        <Navigation navLinks={navLinks} />
+                    </div>
+    
+                    <div className="nav-btn">
+                        <Link href="./pages/register/sgnin.html" className="login">Se connecter</Link>
+                        <Link href="./pages/profile/profile.html" className="register hidden">Profile</Link>
+                    </div>
                 </div>
-            </div>
-        </nav>
-    )
+            </nav>
+        )
+    }
 }
